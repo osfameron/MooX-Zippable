fbaa3224dab06a86b320d70ed5b775a252d8cb09
Hakim Cassimally <hakim@doesliverpool.com>
2014-06-06 18:11:12 +0000

    reinstate error scalar->method (callback ok)

--- lib/MooX/Zippable.pm
+++ lib/MooX/Zippable.pm
@@ -305,10 +305,18 @@ use Carp qw(croak);
 use Moo;
 extends 'MooX::Zipper';
 with 'MooX::Zippable';
+use Scalar::Util 'reftype';
 
 sub go { croak "Can't traverse a scalar" }
 
 sub set { croak "Can't set a scalar key, perhaps you wanted to ->replace?" }
+
+sub call { 
+    my ($self, $code) = @_;
+    croak "Can't call a method on a scalar, try a callback?"
+        unless reftype $code eq 'CODE';
+    return $self->next::method($code);
+}
 
 package MooX::Zipper::Hash;
 use Moo;
